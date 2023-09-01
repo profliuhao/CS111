79d8b6912b137a355acfc5db79dfc516edbeb383
liuha <liuha@montclair.edu>
2023-08-31 23:09:02 -0400

    add output formatting.

--- src/CircleAreaCalculator.java
+++ src/CircleAreaCalculator.java
@@ -6,14 +6,15 @@ public class CircleAreaCalculator {
         Scanner scanner = new Scanner(System.in);
 
         // Prompt the user for the radius of the circle
-        System.out.print("Enter the radius of the circle: ");
+        System.out.print("Enter the radius of the circle (in cm): ");
         double radius = scanner.nextDouble();
 
         // Calculate the area of the circle
         double area = Math.PI * Math.pow(radius, 2);
 
         // Display the calculated area
-        System.out.println("The area of the circle is: " + area);
+        System.out.println("The area of the circle is: " + area + " cm^2");
+        System.out.println("The area of the circle is: " + String.format("%.2f", area) + " cm^2");
 
         // Close the scanner to free up resources
         scanner.close();
